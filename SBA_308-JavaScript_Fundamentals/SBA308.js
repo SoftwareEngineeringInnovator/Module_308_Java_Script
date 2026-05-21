70e84667603bd8a6c18ead76bfddac5ca318f1ad
SoftwareEngineeringInnovator <fredychilito@outlook.com>
2026-05-20 23:09:48 -0400

    pushing updated file with fucntions working as expected

--- SBA_308-JavaScript_Fundamentals/SBA308.js
+++ SBA_308-JavaScript_Fundamentals/SBA308.js
@@ -12,7 +12,7 @@ const AssignmentGroup = {
   group_weight: 25,
   assignments: [
     {
-      id: 4,
+      id: 1,
       name: "Declare a Variable",
       due_at: "2023-01-25",
       points_possible: 50
@@ -76,100 +76,158 @@ const LearnerSubmissions = [
   }
 ];
 
-// FUNCTION TO VALIDATE COURSE.ID comparing GROUP.ID
+// function getLearnerData(course, ag, submissions) {
+//   // here, we would process this data to achieve the desired result.
+//   const result = [
+//     {
+//       id: 125,
+//       avg: 0.985, // (47 + 150) / (50 + 150)
+//       1: 0.94, // 47 / 50
+//       2: 1.0 // 150 / 150
+//     },
+//     {
+//       id: 132,
+//       avg: 0.82, // (39 + 125) / (50 + 150)
+//       1: 0.78, // 39 / 50
+//       2: 0.833 // late: (140 - 15) / 150
+//     }
+//   ];
 
-function validateCourse(course, group) {
+//   return result;
+// }
 
-  if (course.id !== group.course_id) {
-    throw new Error("Invalid Course ID.");
+// const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
+
+// console.log("All Student Result", result);
+
+function getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions) {
+  let studentResult = [];
+
+  try {
+    // let learnerReport = {};
+    if (CourseInfo.id !== AssignmentGroup.course_id) { // 1. CourseIn compare IDs
+      throw new error ("Course ID Is Invalid!");
+    }
+
+    // loop assigments
+    //submit assisgment id
+    // Function to loop throught assigment groups
+    // To get the latennes 10% deduct from submission score
+
+    let assigmentScores = 0;
+    let possiblePoints = 0;
+    let newID = 0;
+    let learnerReport = {};
+
+    // let totalAvg = assigmentScores / possiblePoints
+    for (let i = 0; i < LearnerSubmissions.length; i++) {
+      // break
+
+      let aveAssigment = 0;
+
+      // calling function to get specific assigment information for Posible Points
+      let assigmentInfo = getAssigmentInfo(AssignmentGroup.assignments, LearnerSubmissions[i].assignment_id);
+      // console.log("assigmentInfo", assigmentInfo);
+      // console.log(LearnerSubmissions[i].assignment_id);
+      // break;
+
+      // Validate if assigment is due in the future
+      if (new Date(assigmentInfo.due_at) > new Date() || assigmentInfo.points_possible == 0) {
+        continue;
+      }
+
+      if (assigmentInfo.points_possible == 0) {
+        continue;
+      }
+
+      // console.log("Submission.score:", LearnerSubmissions[0].submission.score);
+      // if (AssignmentGroup.assignment[0].id == LearnerSubmissions[i].assignment_id) { 
+
+      // }
+
+      //Calculate whole assigmentaverage
+
+
+      if (newID != LearnerSubmissions[i].learner_id) {
+        if (i == 0) {
+          //first time learnerReports is empty then do nothing
+        } else {
+          studentResult.splice(1, 0, learnerReport)
+          learnerReport = {};
+        }
+        newID = LearnerSubmissions[i].learner_id;
+        learnerReport["id"] = newID;
+        learnerReport["avg"] = 0;
+        assigmentScores = 0;
+        possiblePoints = 0;
+      }
+
+      //Finding if submitted late -> submitted date > assigment due date
+      let lateness = false;
+      if (new Date(LearnerSubmissions[i].submission.submitted_at) > new Date(assigmentInfo.due_at)) {
+        lateness = true;
+      }
+
+      //Calculate single assigmentaverage / max points and subtract lateness
+      let singleAssigmentScore = 0;
+
+      if (lateness) {
+        singleAssigmentScore = (LearnerSubmissions[i].submission.score - (assigmentInfo.points_possible * 0.1));
+
+        // averageAssigment = (LearnerSubmissions[i].submission.score - (assigmentInfo.points_possible * 0.1)) / assigmentInfo.points_possible;
+        //ID 1: averageAssigment
+      } else {
+        singleAssigmentScore = LearnerSubmissions[i].submission.score;
+
+        // averageAssigment = LearnerSubmissions[i].submission.score / assigmentInfo.points_possible;
+        //ID 1: averageAssigment
+      }
+
+      averageAssigment = singleAssigmentScore / assigmentInfo.points_possible;
+      const assigmentID = assigmentInfo.id;
+      learnerReport[assigmentID] = averageAssigment.toFixed(2);
+
+      // console.log("Single Resultt", learnerReport);
+
+      // caldulate whole total assigment average
+      assigmentScores += singleAssigmentScore;
+      possiblePoints += assigmentInfo.points_possible;
+      let totalAvg = assigmentScores / possiblePoints
+
+      // learrnerReport.avg = totalAvg;
+      // To remove decimal points
+      learnerReport.avg = totalAvg.toFixed(2);
+
+
+      // Create Variable
+      //collect all students reports
+      // studentResult.push(learnerReport);
+      // learnerReport = {};
+      if ((i + 1) == LearnerSubmissions.length) {
+        studentResult.splice(1, 0, learnerReport);
+      }
+    }
+
+
+    return studentResult;
+  } catch (err) {
+    console.log("Error Encountered:", err.message);
   }
-
 }
 
-// TESTING THE VALIDATION FUNCTION
-
-console.log("Validating COURSE and GROUP ID");
-try {
-
-  validateCourse(CourseInfo, AssignmentGroup);
-
-
-  console.log("Course is valid.");
-  // Result by using proper course_id 451
-  // id: 12345,
-  // name: "Fundamentals of JavaScript",
-  // course_id: 451,
-  // group_weight: 25,
-  // assignments: [
-  // Result: Course is valid.
-
-} catch (error) {
-
-  console.error(`Course: "${error.message}"`);
-  // Result by updating course_id from 451 to  452
-  // id: 12345,
-  // name: "Fundamentals of JavaScript",
-  // course_id: 452,
-  // group_weight: 25,
-  // assignments: [
-  // Result: Course: "Invalid Course ID."
-}
-
-// FUNCTION TO FIND ASSIGNMENT
-
-function findAssignment(assignments, assignmentID) {
-
-  // Loop through assignment object list
-  for (const assignment of assignments) {
-
-    // Check assignment's IDs
-    if (assignment.id === assignmentID) {
-      console.log("Assignment Found, name: ", assignment.name);
-      return;
-      //  Results with CORRECT assigmentID 
-      //  Assignment Validation:
-      //  Assignment Write a Function Found assignment.
+// Get single assigment information - Possible Points
+function getAssigmentInfo(assigmentInformation, submitID) {
+  let assigment = {}
+  for (let idx in assigmentInformation) { // for (let idx = 0; i < assigmentInformation.length; i++)
+    // console.log("aInfor" , assigmentInformation);
+    if (assigmentInformation[idx].id == submitID) {
+      assigment = assigmentInformation[idx];
+      break;
+      // retrun assigmentInformation[idx];
     }
   }
-  console.log("Assignment Not Found with ID:", assignmentID);
-  //  Results with INCORRECT assigmentID 99
-  // Assignment Validation:
-  // Assignment Write a Function Found assignment.
+  return assigment;
 }
 
-
-// FUNCTION TO CHECK VALID DATA - number or string
-
-function validData(score, pointsPossible) {
-
-  // Use typeof() and OR to evaluate if the score is not a number, OR points is not a number OR points equals 0
-  if (typeof score !== "number" || typeof pointsPossible !== "number" || pointsPossible === 0) {
-    return false;
-  }
-
-  return true;
-}
-
-// TESTING THE VALIDATION FUNCTION
-
-// console.log("DATA VALIDATION TESTS:");
-// DATA VALIDATION TESTS:
-
-// Case 1: proper data -True
-// console.log("Numbers:", validData(85, 100));
-// Result - Numbers: true
-
-// Case 2: one of the score is a string - False
-// console.log("String:", validData("85", 100));
-// Result - String: false
-
-// console.log("String:", validData(85, "100"));
-// Result - String: false
-
-// Case 3: Zero points possible- False
-// console.log("0 Points Possible:", validData(85, 0));
-// Result - 0 Points Possible: false
-
-// Case 4: Score 0 - True
-// console.log("Score 0", validData(0, 100));
-// Result - Score 0 true
+const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
+console.log("All Student Result:", result);
